f8796adc29ce782f215e568ab3c82883f9f2eb98
angel <angel.yarkiy@gmail.com>
2026-07-06 19:24:10 +0500

    +

--- client/src/components/c-ani-block/c-ani-block.ts
+++ client/src/components/c-ani-block/c-ani-block.ts
@@ -2,10 +2,19 @@ import "./c-ani-block.scss";
 import template from "./c-ani-block.html?raw";
 import { ScrollLinked } from "@/scroll-linked";
 
+type TDoAnimation = {
+  elClassName: string;
+  start?: number;
+  duration?: number;
+  end?: number;
+  onStep: (progress: number, el: HTMLElement) => void;
+};
+
 export class CAniBlock extends HTMLElement {
   private ticking = false;
   wrapEl?: HTMLElement;
   progress = 0;
+  animations: (() => void)[] = [];
 
   connectedCallback() {
     this.classList.add("ani-block");
@@ -31,12 +40,32 @@ export class CAniBlock extends HTMLElement {
   getSubProgress(start: number, end: number) {
     return ScrollLinked.getSubProgress(this.progress, start, end);
   }
+  doAnimation({ elClassName, start, duration, end, onStep }: TDoAnimation) {
+    const el = document.querySelector(elClassName) as HTMLElement;
+    if (!el) return;
+
+    const _start = start ?? 0;
+    let _end;
+    if (duration) _end = _start + duration;
+    else _end = end ?? 1;
+
+    this.animations.push(() => {
+      const subProgress = ScrollLinked.getSubProgress(
+        this.progress,
+        _start,
+        _end,
+      );
+      onStep(subProgress, el);
+    });
+  }
   update() {
     if (!this.wrapEl) return;
     this.progress = ScrollLinked.getPinProgress(this.wrapEl);
     // Apply progress to CSS variable for styling
     this.style.setProperty("--pin-progress", this.progress.toString());
     this.ticking = false;
+
+    this.animations.forEach((fn) => fn());
   }
 }
 customElements.define("c-ani-block", CAniBlock);
--- client/src/components/c-ani/c-ani-start/c-ani-start.ts
+++ client/src/components/c-ani/c-ani-start/c-ani-start.ts
@@ -8,22 +8,111 @@ export class CAniStart extends CAniBlock {
     this.innerHTML = this.innerHTML.replaceAll("{{content}}", template);
     this.update();
     this.init();
+
+    this.doAnimation({
+      elClassName: ".str-down",
+      start: 0,
+      end: 0.2,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateY(${progress * 100}px)`;
+      },
+    });
+    const s = 1000;
+    this.doAnimation({
+      elClassName: ".i01",
+      start: 0,
+      end: 1,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translate(-50%, ${(-progress * s) / 2}px)`;
+      },
+    });
+    this.doAnimation({
+      elClassName: ".i02",
+      start: 0,
+      end: 1,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translate(-50%, ${-progress * s}px)`;
+      },
+    });
+
+    const t_s = 100;
+    this.doAnimation({
+      elClassName: ".t00",
+      start: 0,
+      duration: 0.15,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateY(${-progress * t_s}px)`;
+        elem.style.opacity = `${1 - progress}`;
+      },
+    });
+    this.doAnimation({
+      elClassName: ".t01",
+      start: 0.05,
+      duration: 0.15,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateY(${-progress * t_s}px)`;
+        elem.style.opacity = `${1 - progress}`;
+      },
+    });
+
+    const d00 = 0.15;
+    this.doAnimation({
+      elClassName: ".block-02",
+      start: 0.1,
+      duration: d00 * 4,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateY(${-progress * 500}px)`;
+      },
+    });
+    this.doAnimation({
+      elClassName: ".bl-00",
+      start: 0.1,
+      duration: d00,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateX(${200 - progress * 200}px)`;
+        elem.style.opacity = `${progress}`;
+      },
+    });
+    this.doAnimation({
+      elClassName: ".bl-01",
+      start: 0.1 + d00,
+      duration: d00,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateX(-${200 - progress * 200}px)`;
+        elem.style.opacity = `${progress}`;
+      },
+    });
+    this.doAnimation({
+      elClassName: ".bl-02",
+      start: 0.1 + d00 * 2,
+      duration: d00,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateX(${200 - progress * 200}px)`;
+        elem.style.opacity = `${progress}`;
+      },
+    });
+    this.doAnimation({
+      elClassName: ".bl-03",
+      start: 0.1 + d00 * 3,
+      duration: d00,
+      onStep: (progress, elem: HTMLElement) => {
+        elem.style.transform = `translateX(-${200 - progress * 200}px)`;
+        elem.style.opacity = `${progress}`;
+      },
+    });
   }
-  update(): void {
-    super.update();
+  // update(): void {
+  //   super.update();
 
-    const strDownEl = document.querySelector(".str-down") as HTMLElement;
+  //   const strDownEl = document.querySelector(".str-down") as HTMLElement;
 
-    const v00 = this.getSubProgress(0, 0.2);
+  //   const v00 = this.getSubProgress(0, 0.2);
 
-    strDownEl.style.transform = `translateY(${v00 * 100}px)`;
+  //   const i01 = document.querySelector("") as HTMLElement;
+  //   const i02 = document.querySelector(".i02") as HTMLElement;
+  //   const v01 = this.getSubProgress(0, 1);
 
-    const i01 = document.querySelector(".i01") as HTMLElement;
-    const i02 = document.querySelector(".i02") as HTMLElement;
-    const v01 = this.getSubProgress(0, 1);
-    const s = 100;
-    i01.style.transform = `translate(-50%, ${(-v01 * s) / 2}px)`;
-    i02.style.transform = `translate(-50%, ${-v01 * s}px)`;
-  }
+  //   i02.style.transform = `translate(-50%, ${-v01 * s}px)`;
+  // }
 }
 customElements.define("c-ani-start", CAniStart);
